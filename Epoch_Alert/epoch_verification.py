0358340756439e36de5b3146bdb6c40e8fbbfe61
saswata000 <sas.2106@gmail.com>
2014-03-28 23:07:25 -0400

    Update epoch_verification.py
    Run continously with nohup in the background. This will record current and last good epoch.

--- Epoch_Alert/epoch_verification.py
+++ Epoch_Alert/epoch_verification.py
@@ -1,1 +1,16 @@
-coming soon
+#!/usr/bin/python
+import pyodbc
+import datetime
+import time
+
+
+cnxn = pyodbc.connect('DSN=LV_Vertica')
+now = datetime.date.today()
+cursor_tables = cnxn.cursor()
+cursor_insert = cnxn.cursor()
+
+while 1:
+	cursor_insert.execute("insert into vertica_audit.epoch_verification SELECT current_timestamp ,last_good_epoch, current_epoch, ahm_epoch, wos_row_count, ros_row_count, wos_used_bytes, ros_used_bytes from system")
+	cnxn.commit()
+	print "Done"
+	time.sleep(5)
