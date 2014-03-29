b380dfffe5f138cf0672435bce55f2113feff051
saswata000 <sas.2106@gmail.com>
2014-03-28 23:01:14 -0400

    Update epoch_alert.py
    Continuously running epoch alert, customize to add email and epoch interval difference(currently set to 500) and sleep time. Execute this script with nohup in the background and it will send alert when current and last know epoch falls behind.

--- Epoch_Alert/epoch_alert.py
+++ Epoch_Alert/epoch_alert.py
@@ -1,1 +1,23 @@
-Coming Soon
+#!/usr/bin/python
+import pyodbc
+import datetime
+import time
+
+cnxn = pyodbc.connect('DSN=LV_Vertica')
+now = datetime.date.today()
+cursor_tables = cnxn.cursor()
+sql="select epoch_timestamp,last_good_epoch, current_epoch, ahm_epoch, wos_row_count, ros_row_count, wos_used_bytes, ros_used_bytes from vertica_audit.epoch_verification where epoch_timestamp = (select max(epoch_timestamp) from vertica_audit.epoch_verification);"
+
+print sql
+
+while 1:
+        pass
+        for row in cursor_tables.execute(sql) :
+
+                if row.last_good_epoch - row.current_epoch >= 500 :
+                        print "Alert epoch failing behind"
+                else :
+                        print "Nothing to worry !!"
+        time.sleep(10)
+        #cnxn.close()
+
